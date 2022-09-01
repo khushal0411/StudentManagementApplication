00058f5fc53c7731de0edaf17997528d20061016
Joy N Patel <joypatel242001@gmail.com>
2022-09-01 21:51:01 +0530

    Package created for efficiency, log in screen added for student and teacher, dialog box add (inflate is remaining) and home screen added. Moreover, readme.txt file is added in every package.

---
+++ app/src/main/java/com/example/student_management_system/SplashAndIntro/Activity/IntroScreen.kt
@@ -0,0 +1,12 @@
+package com.example.student_management_system.SplashAndIntro.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.student_management_system.R
+
+class IntroScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro_screen)
+    }
+}
