3b83d3f25477ed0109bd13094eaa6c30ee28a4b6
Joy N Patel <joypatel242001@gmail.com>
2022-09-11 14:45:58 +0530

    UI updated for toolbar

--- app/src/main/java/com/example/student_management_system/HomeScreen/Activity/HomeScreen.kt
+++ app/src/main/java/com/example/student_management_system/HomeScreen/Activity/HomeScreen.kt
@@ -2,6 +2,9 @@ package com.example.student_management_system.HomeScreen.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.student_management_system.R
 import com.example.student_management_system.databinding.ActivityHomeScreenBinding
@@ -13,8 +16,15 @@ class HomeScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen)
         setContentView(binding.root)
+
+
         setSupportActionBar(binding.toolbar.tbMain)
         Objects.requireNonNull(supportActionBar)!!.setDisplayShowTitleEnabled(false)
         binding.toolbar.tbMain.title=getString(R.string.app_name)
+
+        val window: Window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.transparent)
     }
 }
--- app/src/main/java/com/example/student_management_system/repository/APIRepository.kt
+++ app/src/main/java/com/example/student_management_system/repository/APIRepository.kt
@@ -1,7 +1,7 @@
 package com.example.student_management_system.repository
 
-import com.example.student_management_system.RetrofitInterface
 
-class APIRepository(val retrofitInterface: RetrofitInterface) {
+
+class APIRepository {
 
 }
