41b124b7655090041ec87720a4e799876e3485d3
Joy N Patel <joypatel242001@gmail.com>
2022-09-12 12:20:31 +0530

    registration form complete for student and teacher.

---
+++ app/src/main/java/com/example/student_management_system/Admin/Activity/FessSubjectRegistration.kt
@@ -0,0 +1,12 @@
+package com.example.student_management_system.Admin.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.student_management_system.R
+
+class FessSubjectRegistration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fess_subject_registration)
+    }
+}
---
+++ app/src/main/java/com/example/student_management_system/Admin/Activity/RegisterNewTeacher.kt
@@ -0,0 +1,12 @@
+package com.example.student_management_system.Admin.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.student_management_system.R
+
+class RegisterNewTeacher : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_new_teacher)
+    }
+}
